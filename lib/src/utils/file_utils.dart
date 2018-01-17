26dcf8381bab9a5c992b5cd8fd90d3dce5b9c988
thumbert <humbert.tony@yahoo.com>
2018-01-17 15:45:23 -0500

    list files

--- lib/src/utils/file_utils.dart
+++ lib/src/utils/file_utils.dart
@@ -1,5 +1,6 @@
 
 import 'dart:io';
+import 'package:path/path.dart';
 
 /// Get the last file updated in the directory.  Does not follow links or
 /// search recursively.  It can be slow ...
@@ -7,4 +8,14 @@ File latestFile(Directory dir) {
   List files = dir.listSync().where((entity) => entity is File).toList();
   files.sort((File a, File b) => a.lastModifiedSync().compareTo(b.lastModifiedSync()));
   return files.last;
+}
+
+/// List all the files from a directory that contain a pattern in the filename. 
+List<File> listFiles(Directory dir, {Pattern pattern}) {
+  List files = dir.listSync()
+      .where((entity) => entity is File)
+      .where((entity) => basenameWithoutExtension((entity as File).path).contains(pattern))
+      .toList();
+  files.sort((File a, File b) => a.path.compareTo(b.path));
+  return files;
 }
