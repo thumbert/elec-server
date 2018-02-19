ad872e0a31596d4740e7bbf930700730442507b1
thumberts <tony.humberts@gmail.com>
2018-02-19 11:27:59 -0500

    wrote to ISO time stamp function

--- lib/src/utils/iso_timestamp.dart
+++ lib/src/utils/iso_timestamp.dart
@@ -5,20 +5,18 @@ import 'package:timezone/standalone.dart';
 
 Location _eastern = getLocation('US/Eastern');
 
-
-
 /// Convert from an ISONE string tuple.
 /// [localDate] is a String in format 'mm/dd/yyyy'
 /// [hourEnding] is of the form '01', '02', '02X', '03', ... '24'
 /// Return an hour beginning UTC DateTime
 TZDateTime parseHourEndingStamp(String localDate, String hourEnding) {
-  // the result is an hour beginning
   TZDateTime res = new TZDateTime(
-      _eastern,
-      int.parse(localDate.substring(6, 10)),
-      int.parse(localDate.substring(0, 2)),
-      int.parse(localDate.substring(3, 5)),
-      int.parse(hourEnding.substring(0, 2)) - 1).toUtc();
+          _eastern,
+          int.parse(localDate.substring(6, 10)),
+          int.parse(localDate.substring(0, 2)),
+          int.parse(localDate.substring(3, 5)),
+          int.parse(hourEnding.substring(0, 2)) - 1)
+      .toUtc();
 
   if (hourEnding == '02X') {
     // you are at a transition point
@@ -29,27 +27,53 @@ TZDateTime parseHourEndingStamp(String localDate, String hourEnding) {
 }
 
 /// Convert an hour beginning timestamp to the ISO hour ending format.
+/// The [hourBeginning] timestamp needs to be in the eastern time zone.
 /// Return a two element list with the date and hour ending.
-/// E.g. ['']
-List<String> toIsoHourEndingFormat(TZDateTime hourBeginning) {
+/// E.g. ['2018-02-01', '03']
+List<String> toIsoHourEndingStamp(TZDateTime hourBeginning) {
+  Hour hour = new Hour.beginning(hourBeginning);
+  int offsetStart = hour.start.timeZoneOffset.inHours;
+  bool isFallBack = isFallBackDate(new Date(
+      hourBeginning.year, hourBeginning.month, hourBeginning.day));
+  List res = [hourBeginning.toString().substring(0, 10)];
 
+  if (isFallBack) {
+   if (hour.start.hour == 0) res.add('01');
+   else if (hour.start.hour == 1 && offsetStart == -4) res.add('02');
+   else if (hour.start.hour == 1 && offsetStart == -5) res.add('02X');
+   else res.add(hour.end.hour.toString().padLeft(2, '0'));
+  } else {
+    res.add(hour.end.hour.toString().padLeft(2, '0'));
+  }
+
+  return res;
 }
 
-
+/// Check if this date is a fall back date.  Return true if it is.
+bool isFallBackDate(Date date) {
+  int offsetStart = new TZDateTime(_eastern, date.year, date.month, date.day)
+      .timeZoneOffset
+      .inHours;
+  int offsetEnd = new TZDateTime(_eastern, date.year, date.month, date.day, 23)
+      .timeZoneOffset
+      .inHours;
+  bool res = false;
+  if (offsetStart == -4 && offsetEnd == -5) res = true;
+  return res;
+}
 
 /// When you read the reports with csv, the hour ending is an integer.
 /// Fix it with this function.
 String stringHourEnding(dynamic hourEnding) {
   if (hourEnding is int) {
-    hourEnding = hourEnding.toString().padLeft(2,'0');
+    hourEnding = hourEnding.toString().padLeft(2, '0');
   }
   return hourEnding;
 }
 
-
 /// Convert this date to the ISONE preferred date format, e.g. 11/26/2016.
 String mmddyyyy(Date date) {
-  String mm = date.month.toString().padLeft(2,'0');
-  String dd = date.day.toString().padLeft(2,'0');
+  String mm = date.month.toString().padLeft(2, '0');
+  String dd = date.day.toString().padLeft(2, '0');
   return '$mm/$dd/${date.year}';
 }
--- test/utils/iso_timestamp_test.dart
+++ test/utils/iso_timestamp_test.dart
@@ -2,47 +2,85 @@ library test.elec.iso_parsetime;
 
 import 'package:test/test.dart';
 import 'package:timezone/standalone.dart';
+import 'package:date/date.dart';
 import 'package:elec_server/src/utils/timezone_utils.dart';
 import 'package:elec_server/src/utils/iso_timestamp.dart';
 
-
-
 test_parseIsoTimestamp() {
+  Location location = getLocation('US/Eastern');
 
+  List<List> dts = [
+    ['03/08/2015', '01', new TZDateTime.utc(2015, 3, 8, 6)],
+    ['03/08/2015', '03', new TZDateTime.utc(2015, 3, 8, 7)],
+    ['03/08/2015', '04', new TZDateTime.utc(2015, 3, 8, 8)],
+    ['03/08/2015', '05', new TZDateTime.utc(2015, 3, 8, 9)],
+    ['11/01/2015', '01', new TZDateTime.utc(2015, 11, 1, 5)],
+    ['11/01/2015', '02', new TZDateTime.utc(2015, 11, 1, 6)],
+    ['11/01/2015', '02X', new TZDateTime.utc(2015, 11, 1, 7)],
+    ['11/01/2015', '03', new TZDateTime.utc(2015, 11, 1, 8)],
+    ['11/01/2015', '04', new TZDateTime.utc(2015, 11, 1, 9)],
+    ['11/02/2015', '01', new TZDateTime.utc(2015, 11, 2, 6)],
+    ['11/02/2015', '02', new TZDateTime.utc(2015, 11, 2, 7)],
+    ['11/02/2015', '03', new TZDateTime.utc(2015, 11, 2, 8)],
+  ];
 
   test('Parse ISO timestamp', () {
-    List dts = [
-      ['03/08/2015', '01', new TZDateTime.utc(2015, 3, 8, 6)],
-      ['03/08/2015', '03', new TZDateTime.utc(2015, 3, 8, 7)],
-      ['03/08/2015', '04', new TZDateTime.utc(2015, 3, 8, 8)],
-      ['03/08/2015', '05', new TZDateTime.utc(2015, 3, 8, 9)],
-
-      ['11/01/2015', '01', new TZDateTime.utc(2015, 11, 1, 5)],
-      ['11/01/2015', '02', new TZDateTime.utc(2015, 11, 1, 6)],
-      ['11/01/2015', '02X', new TZDateTime.utc(2015, 11, 1, 7)],
-      ['11/01/2015', '03', new TZDateTime.utc(2015, 11, 1, 8)],
-      ['11/01/2015', '04', new TZDateTime.utc(2015, 11, 1, 9)],
-
-      ['11/02/2015', '01', new TZDateTime.utc(2015, 11, 2, 6)],
-      ['11/02/2015', '02', new TZDateTime.utc(2015, 11, 2, 7)],
-      ['11/02/2015', '03', new TZDateTime.utc(2015, 11, 2, 8)],
-    ];
-
-    List res = [];
-    dts.forEach((List inp) {
+    List res = dts.map((List inp) {
       DateTime hb = parseHourEndingStamp(inp[0], inp[1]);
-      return res.add({
-        'input': inp.take(2).join(' '),
-        'utc_HB': hb,
-        'utc_HE': inp[2]
-      });
-    });
-    //res.forEach((e) => print(e));
-    expect(dts.map((e)=>e[2]).toList(), res.map((e)=>e['utc_HB'].add(new Duration(hours: 1))).toList());
+      return {'input': inp.take(2).join(' '), 'utc_HB': hb, 'utc_HE': inp[2]};
+    }).toList();
+    expect(dts.map((e) => e[2]).toList(),
+        res.map((e) => e['utc_HB'].add(new Duration(hours: 1))).toList());
   });
+
+  test('to ISO timestamp, spring ahead', () {
+    List<Hour> hB = new Interval(new TZDateTime(location, 2015, 3, 8),
+            new TZDateTime(location, 2015, 3, 8, 5))
+        .splitLeft((dt) => new Hour.beginning(dt));
+    List out = [
+      ['2015-03-08', '01'],
+      ['2015-03-08', '03'],
+      ['2015-03-08', '04'],
+      ['2015-03-08', '05'],
+    ];
+    for (int i=0; i<4; i++) {
+      expect(toIsoHourEndingStamp(hB[i].start), out[i]);
+    }
+  });
+
+  test('to ISO timestamp, fall back', () {
+    List<Hour> hB = new Interval(new TZDateTime(location,2015,11,1),
+        new TZDateTime(location,2015,11,1,4))
+        .splitLeft((dt) => new Hour.beginning(dt));
+    List out = [
+      ['2015-11-01', '01'],
+      ['2015-11-01', '02'],
+      ['2015-11-01', '02X'],
+      ['2015-11-01', '03'],
+      ['2015-11-01', '04'],
+    ];
+    for (int i=0; i<5; i++) {
+      expect(toIsoHourEndingStamp(hB[i].start), out[i]);
+    }
+  });
+
+  test('to ISO timestamp, regular day', () {
+    List<Hour> hB = new Interval(new TZDateTime(location,2015,1,1),
+        new TZDateTime(location,2015,1,1,4))
+        .splitLeft((dt) => new Hour.beginning(dt));
+    List out = [
+      ['2015-01-01', '01'],
+      ['2015-01-01', '02'],
+      ['2015-01-01', '03'],
+      ['2015-01-01', '04'],
+    ];
+    for (int i=0; i<out.length; i++) {
+      expect(toIsoHourEndingStamp(hB[i].start), out[i]);
+    }
+  });
+
+
 }
-
-
 
 main() async {
   await initializeTimeZone(getLocationTzdb());
