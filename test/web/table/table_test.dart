f5f086b8b50391ca65750a68b1efe4207227e582
thumbert <tony.humberts@gmail.com>
2018-10-26 15:03:06 -0400

    Update table_test.dart

--- test/web/table/table_test.dart
+++ test/web/table/table_test.dart
@@ -1,20 +1,21 @@
 
+
 import 'dart:html';
 import 'package:intl/intl.dart';
 import 'package:elec_server/src/utils/html_table.dart';
 
 
 simpleTable() {
-  List<Map> data = [
+  var data = <Map<String,dynamic>>[
     {'airport': 'BOS', 'tmin': '22', 'tmax': '45'},
     {'airport': 'BWI', 'tmin': '27', 'tmax': '49'},
     {'airport': 'LGA', 'tmin': '25', 'tmax': '47'},
   ];
-  new Table(querySelector('#wrapper-simple-table'), data);
+  new HtmlTable(querySelector('#wrapper-simple-table'), data);
 }
 
 tableWithFormat() {
-  List<Map> data = [
+  var data = <Map<String,dynamic>>[
     {'date': new DateTime(2018,1), 'price': 85.24},
     {'date': new DateTime(2018,2), 'price': 73.1},
     {'date': new DateTime(2018,3), 'price': 55.93},
@@ -24,20 +25,18 @@ tableWithFormat() {
   var fmt = new DateFormat('MMMyy');
   var dollar = new NumberFormat.currency(symbol: '\$');
 
-  Map options = {
+  var options = <String,dynamic>{
     'date': {
       'valueFormat': (DateTime dt) => fmt.format(dt)
     },
     'price': {
-      'valueFormat': (num x) => dollar.format(x)
+      'valueFormat': (x) => dollar.format(x)
     }
   };
-  new Table(querySelector('#wrapper-table-format'), data, options: options);
+  new HtmlTable(querySelector('#wrapper-table-format'), data, options: options);
 
   options['rowNumbers'] = true;
-  new Table(querySelector('#wrapper-table-format-rownumbers'), data, options: options);
-
-
+  new HtmlTable(querySelector('#wrapper-table-format-rownumbers'), data, options: options);
 }
 
 
@@ -45,4 +44,5 @@ tableWithFormat() {
 main() {
   simpleTable();
   tableWithFormat();
+
 }
