1a51c6866caca45d10af49e60f86f9773ae5448d
thumbert <tony.humberts@gmail.com>
2019-02-05 15:38:55 -0500

    Update api_customer_counts_eversource.dart

--- lib/api/utilities/api_customer_counts_eversource.dart
+++ lib/api/utilities/api_customer_counts_eversource.dart
@@ -3,29 +3,35 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:rpc/rpc.dart';
-import '../../src/utils/api_response.dart';
+import 'package:elec_server/src/utils/api_response.dart';
 
 
-@ApiClass(name: 'eversource', version: 'v1')
+@ApiClass(name: 'utility', version: 'v1')
 class ApiCustomerCounts {
-  DbCollection coll;
-  String collectionName = 'customer_counts_ct';
+  DbCollection coll1;
 
   ApiCustomerCounts(Db db) {
-    coll = db.collection(collectionName);
+    coll1 = db.collection('eversource_customer_counts');
   }
 
-  /// return the historical counts and usage by rate class
-  @ApiMethod(path: 'customercounts/ct')
-  Future<ApiResponse> customerCountsCt() async {
+  /// get available zones
+  @ApiMethod(path: 'customercounts/eversource/zones')
+  Future<List<String>> eversourceZones() async {
+    var rows = await coll1.distinct('zone');
+    var out = (rows['values'] as List).cast<String>();
+    return out..sort();
+  }
+
+  /// return the historical counts and usage by zone
+  @ApiMethod(path: 'customercounts/eversource/zone/{zone}')
+  Future<ApiResponse> eversourceCustomerCounts(String zone) async {
     SelectorBuilder query = where;
-//    query = query.eq('town', town);
-//    query = query.eq('variable', 'kWh');
+    query.eq('zone', zone);
     query = query.excludeFields(['_id']);
-    var res = await coll.find(query).toList();
+    var res = await coll1.find(query).toList();
     return ApiResponse()..result = json.encode(res);
   }
-
 }
+
 
 
