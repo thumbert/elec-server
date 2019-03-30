38f83003e6a45ffca92fe963c52aa678bf9a78f7
thumberts <tony.humberts@gmail.com>
2019-03-30 12:41:56 -0400

    fix textinput placeholder

--- lib/src/ui/text_input.dart
+++ lib/src/ui/text_input.dart
@@ -6,17 +6,18 @@ class TextInput {
   html.Element wrapper;
   html.TextInputElement _textInput;
   String name;
-  String defaultValue;
+  String initialValue;
 
   /// A simple text input with a label.
   ///
   /// Variable [name] is the text of the accompanying label.
   ///
   /// Need to trigger an action onChange.
-  TextInput(this.wrapper, this.name, {this.defaultValue, int size}) {
+  TextInput(this.wrapper, this.name, {this.initialValue, int size,
+    String placeholder}) {
 
     var aux = '';
-    if (defaultValue != null) aux = defaultValue;
+    if (initialValue != null) aux = initialValue;
 
     var _wrapper = html.DivElement()
       ..setAttribute('style', 'margin-top: 8px');
@@ -24,8 +25,8 @@ class TextInput {
       ..text = '$name');
     _textInput = html.TextInputElement()
       ..setAttribute('style', 'margin-left: 15px')
-      ..placeholder = aux
       ..value = aux;
+    if (placeholder != null) _textInput.placeholder = placeholder;
     if (size != null) _textInput.size = size;
     _wrapper.children.add(_textInput);
 
