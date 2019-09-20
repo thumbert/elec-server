29606666f2f70282478de1af45ee2ba07b84ce2f
thumbert <tony.humberts@gmail.com>
2019-09-20 13:29:31 -0400

    Update sd_arrawdsum.dart

--- lib/src/db/mis/sd_arrawdsum.dart
+++ lib/src/db/mis/sd_arrawdsum.dart
@@ -14,13 +14,14 @@ class SdArrAwdSumArchive extends mis.MisReportArchive {
     dbConfig ??= ComponentConfig()
       ..host = '127.0.0.1'
       ..dbName = 'mis';
-    dbConfig.collectionName = 'sd_arrawdsum';
+    dbConfig.collectionName = reportName.toLowerCase();
   }
 
   /// the entire tab is one document
   Map<String, dynamic> rowConverter(
-      List<Map> rows, Date reportDate, DateTime version) {
+      List<Map> rows, String account, Date reportDate, String version) {
     var document = <String, dynamic>{};
+    document['account'] = account;
     document['month'] = reportDate.toString().substring(0,7);  /// yyyy-mm
     document['version'] = version;
     var columns = rows.first.keys.skip(1);
@@ -35,10 +36,11 @@ class SdArrAwdSumArchive extends mis.MisReportArchive {
   @override
   Map<int, List<Map<String, dynamic>>> processFile(File file) {
     var report = mis.MisReport(file);
+    var account = report.accountNumber();
     var reportDate = report.forDate();
-    var version = report.timestamp();
+    var version = report.timestamp().toIso8601String();
     var rows = mis.readReportTabAsMap(file, tab: 0);
-    var data0 = rowConverter(rows, reportDate, version);
+    var data0 = rowConverter(rows, account, reportDate, version);
     return {
       0: [data0]
     };
@@ -46,15 +48,17 @@ class SdArrAwdSumArchive extends mis.MisReportArchive {
 
   Future<int> insertTabData(List<Map<String,dynamic>> data, {int tab: 0}) async {
     if (data.isEmpty) return Future.value(null);
+    var account = data.first['account'];
     var date = data.first['month'];
     var version = data.first['version'];
     try {
       await dbConfig.coll.remove({
+        'account': account,
         'month': date,
         'version': version,
       });
       await dbConfig.coll.insertAll(data);
-      print('--->  Inserted $reportName for $date, version $version, tab $tab successfully');
+      print('--->  Inserted $reportName for account $account, month $date, version $version, tab $tab successfully');
       return Future.value(0);
     } catch (e) {
       print('XXX ' + e.toString());
@@ -67,7 +71,8 @@ class SdArrAwdSumArchive extends mis.MisReportArchive {
   Future<Null> setupDb() async {
     await dbConfig.db.open();
     await dbConfig.db.createIndex(dbConfig.collectionName,
-        keys: {'month': 1, 'Location ID': 1, 'version': 1}, unique: true);
+        keys: {'account': 1,
+      'month': 1, 'Location ID': 1, 'version': 1});
     await dbConfig.db.close();
   }
 
