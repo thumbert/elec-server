ed05059bb1a04c741d740c3277f9377705ee747c
thumbert <tony.humberts@gmail.com>
2019-11-12 09:54:26 -0500

    Update rt_system_demand_hourly.dart

--- lib/src/db/isoexpress/rt_system_demand_hourly.dart
+++ lib/src/db/isoexpress/rt_system_demand_hourly.dart
@@ -2,7 +2,7 @@ library db.isoexpress.rt_system_demand_hourly;
 
 import 'dart:io';
 import 'dart:async';
-import 'package:mongo_dart/mongo_dart.dart';
+import 'package:mongo_dart/mongo_dart.dart' as mongo;
 import 'package:timezone/timezone.dart';
 import 'package:date/date.dart';
 import 'package:elec_server/src/db/config.dart';
