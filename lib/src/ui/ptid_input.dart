d055c145326e0f73a0812ff73651ae95c1cb9069
thumberts <tony.humberts@gmail.com>
2021-06-10 08:47:06 -0400

    fix ptid_input

--- lib/src/ui/ptid_input.dart
+++ lib/src/ui/ptid_input.dart
@@ -5,13 +5,12 @@ import 'dart:html' as html;
 class PtidInput {
   html.Element wrapper;
   String name;
-  int initialValue;
-  Map<int,String> ptidToName;
+  int? initialValue;
+  Map<int, String> ptidToName;
   bool canBeEmpty;
 
   late html.TextInputElement _textInput;
   late html.LabelElement _nodeLabel;
-
 
   /// A ptid input field.
   /// [initialValue] can be null, for an empty initial value.
@@ -19,13 +18,11 @@ class PtidInput {
   ///
   /// Need to trigger an action onDataChange.
   PtidInput(this.wrapper, this.initialValue, this.ptidToName,
-      {int size: 6, this.name: 'Ptid', this.canBeEmpty: false}) {
+      {int size = 6, this.name = 'Ptid', this.canBeEmpty = false}) {
+    var aux = initialValue == null ? '' : initialValue.toString();
 
-    var aux = initialValue == null ? '': initialValue.toString();
-
-    var _wrapper = new html.DivElement()
-      ..setAttribute('style', 'margin-top: 8px');
-    _wrapper.children.add(new html.LabelElement()
+    var _wrapper = html.DivElement()..setAttribute('style', 'margin-top: 8px');
+    _wrapper.children.add(html.LabelElement()
       ..text = '$name'
       ..setAttribute('style', 'margin-left: 15px'));
     _textInput = html.TextInputElement()
@@ -47,16 +44,19 @@ class PtidInput {
     if (_textInput.value!.isEmpty) {
       aux = null;
       if (!canBeEmpty) {
-        _textInput.setAttribute('style', 'margin-left: 15px; border: 2px solid red;');
+        _textInput.setAttribute(
+            'style', 'margin-left: 15px; border: 2px solid red;');
       }
     } else {
       aux = int.parse(_textInput.value!);
-      _textInput.setAttribute('style', 'margin-left: 15px; border-color: initial;');
+      _textInput.setAttribute(
+          'style', 'margin-left: 15px; border-color: initial;');
     }
     var text = ptidToName[aux!];
     if (aux != null && text == null) {
       // name not in the ptidToName map
-      _textInput.setAttribute('style', 'margin-left: 15px; border: 2px solid red;');
+      _textInput.setAttribute(
+          'style', 'margin-left: 15px; border: 2px solid red;');
       _nodeLabel.text = '';
     } else {
       // you can set the label
@@ -69,5 +69,4 @@ class PtidInput {
   void onChange(Function x) {
     _textInput.onChange.listen(x as void Function(html.Event)?);
   }
-
 }
