dbf94566e971abc295cdd11a70fa1a570803fbd3
thumberts <tony.humberts@gmail.com>
2022-06-09 12:54:56 -0400

    integer range parsing

--- lib/src/utils/parse_custom_integer_range.dart
+++ lib/src/utils/parse_custom_integer_range.dart
@@ -1,26 +1,36 @@
 library utils.parse_custom_integer_range;
 
-/// Do the opposite of [unpackIntegerList].  Go from an integer list
-/// to a [String], e.g. '1-5, 8, 11-13'.
-String packIntegerList(List<int> x) {
+/// Do the opposite of [unpackIntegerList].  Go from a list of positive
+/// integers to a [String], e.g. '1-5, 8, 11-13'.
+/// The empty list is packed as ''.
+/// Input list [x] does not need to be sorted in order to allow packing of
+/// [11, 12, 1, 2, 3] as '11-3'.
+///
+/// Note: No checks are made to ensure that the list values are between
+/// min and max value.
+String packIntegerList(List<int> x, {int? minValue, int? maxValue}) {
   if (x.isEmpty) return '';
   if (x.length == 1) return x.first.toString();
-  x.sort();
   var out = <String>[];
   var current = x.first;
   for (var i = 1; i < x.length; i++) {
-    if (x[i] == x[i - 1] + 1) {
+    if (x[i] == x[i - 1] + (minValue ?? 1)) {
+      // run continues
       continue;
     } else {
       // a new run
       if (current == x[i - 1]) {
         out.add('$current');
       } else {
+        if (x[i - 1] == maxValue) {
+          continue;
+        }
         out.add('$current-${x[i - 1]}');
       }
       current = x[i];
     }
   }
+  // add the last group
   if (current == x.last) {
     out.add('$current');
   } else {
@@ -29,25 +39,47 @@ String packIntegerList(List<int> x) {
   return out.join(', ');
 }
 
+// String packIntegerRange(List<int> x, {int? minValue, int? maxValue}) {
+//   if (x.isEmpty) return '';
+//   if (x.length == 1) return x.first.toString();
+//   var out = <String>[];
+//   var current = x.first;
+//   for (var i = 1; i < x.length; i++) {
+//     if (x[i] == x[i - 1] + 1) {
+//       continue;
+//     } else {
+//       // a new run
+//       if (current == x[i - 1]) {
+//         out.add('$current');
+//       } else {
+//         out.add('$current-${x[i - 1]}');
+//       }
+//       current = x[i];
+//     }
+//   }
+//   // add the last group
+//   if (current == x.last) {
+//     out.add('$current');
+//   } else {
+//     out.add('$current-${x.last}');
+//   }
+//   return out.join(', ');
+// }
+
 @Deprecated('Use unpackIntegerList')
 List<int> parseCustomIntegerRange(String x) => unpackIntegerList(x);
 
 /// Parse a custom positive integer range, similar to Chrome's custom pages
 /// print format e.g. '1-5, 8, 11-13', etc.
+/// Also, allow parsing of a range not sorted, e.g. '11-3' to represent Nov-Mar
 /// Useful for UI's
-List<int> unpackIntegerList(String x) {
+List<int> unpackIntegerList(String x, {int? minValue, int? maxValue}) {
   if (x == '') return <int>[];
   var rs = x.split(',').map((e) => e.trim());
   var out = <int>[];
   for (var r in rs) {
     if (r.contains('-')) {
-      var aux = r.split('-');
-      if (aux.length != 2) {
-        throw ArgumentError('Problem parsing range $r from $x');
-      }
-      var start = int.parse(aux[0]);
-      var end = int.parse(aux[1]);
-      out.addAll(List.generate(end - start + 1, (i) => i + start));
+      out.addAll(unpackIntegerRange(r, minValue: minValue, maxValue: maxValue));
     } else {
       var aux = int.parse(r);
       if (aux < 0) {
@@ -56,6 +88,28 @@ List<int> unpackIntegerList(String x) {
       }
       out.add(int.parse(r));
     }
+  }
+  return out;
+}
+
+/// Parse '2-6', or '11-3'.
+/// For a range of months of the year, [minValue] = 1, [maxValue] = 12.
+List<int> unpackIntegerRange(String x, {int? minValue, int? maxValue}) {
+  var out = <int>[];
+  var aux = x.split('-');
+  if (aux.length != 2) {
+    throw ArgumentError('Problem parsing range $x');
+  }
+  var start = int.parse(aux[0]);
+  var end = int.parse(aux[1]);
+  if (end < start) {
+    if (maxValue == null || minValue == null) {
+      throw ArgumentError('minValue and maxValue both need to be non-null');
+    }
+    out.addAll(List.generate(maxValue - start + 1, (i) => i + start));
+    out.addAll(List.generate(end - minValue + 1, (i) => i + minValue));
+  } else {
+    out.addAll(List.generate(end - start + 1, (i) => i + start));
   }
   return out;
 }
--- test/utils/parse_custom_integer_range_test.dart
+++ test/utils/parse_custom_integer_range_test.dart
@@ -17,11 +17,25 @@ void tests() {
     test('parse single number into a one element list', () {
       expect(unpackIntegerList('8'), <int>[8]);
     });
-    test('pack integer list', () {
+    test('unpack integer range with min,max', () {
+      expect(unpackIntegerRange('11-3', minValue: 1, maxValue: 12),
+          [11, 12, 1, 2, 3]);
+    });
+    test('parse "11-3, 5-6, 9" with min max', () {
+      expect(unpackIntegerList('11-3, 5-6, 9', minValue: 1, maxValue: 12),
+          [11, 12, 1, 2, 3, 5, 6, 9]);
+    });
+    test('pack an integer list, no wrapping', () {
       expect(packIntegerList([1, 2, 3, 4, 5, 8, 11, 12, 13, 22]),
           '1-5, 8, 11-13, 22');
       expect(
           packIntegerList([1, 2, 3, 4, 5, 8, 11, 13, 14]), '1-5, 8, 11, 13-14');
+    });
+    test('pack an integer list with min, max', () {
+      expect(
+          packIntegerList([11, 12, 1, 2, 3, 6, 8, 9],
+              minValue: 1, maxValue: 12),
+          '11-3, 6, 8-9');
     });
   });
 }
