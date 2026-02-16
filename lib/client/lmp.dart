9ca52734002cacc32acad550863c1bf31b3fffbc
thumbert <tony.humberts@gmail.com>
2026-02-16 10:08:36 -0500

    mess up again with lmp client!

--- lib/client/lmp.dart
+++ lib/client/lmp.dart
@@ -7,6 +7,73 @@ import 'package:elec/risk_system.dart';
 import 'package:timeseries/timeseries.dart';
 import 'package:timezone/timezone.dart';
 
+Future<TimeSeries<num>> getHourlyLmpCaiso(
+    {required Market market,
+    required String locationName,
+    required LmpComponent component,
+    required Term term,
+    required String rustServer}) async {
+  final url = '$rustServer/caiso/prices/${market.name.toLowerCase()}/hourly/'
+      'start/${term.startDate.toString()}/end/${term.endDate.toString()}'
+      '?node_ids=$locationName&components=${component.shortName()}';
+  var response = await http.get(Uri.parse(url));
+  var data = json.decode(response.body) as List;
+  return TimeSeries.fromIterable(data.map((e) => IntervalTuple<num>(
+      Hour.beginning(
+          TZDateTime.parse(Caiso.location, e['hour_beginning'])),
+      e['price'])));
+}
+
+Future<TimeSeries<num>> getHourlyLmpIeso(
+    {required Market market,
+    required String locationName,
+    required LmpComponent component,
+    required Term term,
+    required String rustServer}) async {
+  final url = '$rustServer/ieso/prices/${market.name.toLowerCase()}/hourly/'
+      'start/${term.startDate.toString()}/end/${term.endDate.toString()}'
+      '?locations=$locationName&components=${component.shortName()}';
+  var response = await http.get(Uri.parse(url));
+  var data = json.decode(response.body) as List;
+  return TimeSeries.fromIterable(data.map((e) => IntervalTuple<num>(
+      Hour.beginning(
+          TZDateTime.parse(Ieso.location, e['hour_beginning'])),
+      e['price'])));
+}
+
+Future<TimeSeries<num>> getHourlyLmpIsone(
+    {required Market market,
+    required int ptid,
+    required LmpComponent component,
+    required Term term,
+    required String rustServer}) async {
+  final url = '$rustServer/isone/prices/${market.name.toLowerCase()}/hourly/'
+      'start/${term.startDate.toString()}/end/${term.endDate.toString()}'
+      '?ptids=$ptid&components=${component.shortName()}';
+  var response = await http.get(Uri.parse(url));
+  var data = json.decode(response.body) as List;
+  return TimeSeries.fromIterable(data.map((e) => IntervalTuple<num>(
+      Hour.beginning(
+          TZDateTime.parse(IsoNewEngland.location, e['hour_beginning'])),
+      e['price'])));
+}
+
+Future<TimeSeries<num>> getHourlyLmpNyiso(
+    {required Market market,
+    required int ptid,
+    required LmpComponent component,
+    required Term term,
+    required String rustServer}) async {
+  final url = '$rustServer/nyiso/prices/${market.name.toLowerCase()}/hourly/'
+      'start/${term.startDate.toString()}/end/${term.endDate.toString()}'
+      '?ptids=$ptid&components=${component.shortName()}';
+  var response = await http.get(Uri.parse(url));
+  var data = json.decode(response.body) as List;
+  return TimeSeries.fromIterable(data.map((e) => IntervalTuple<num>(
+      Hour.beginning(
+          TZDateTime.parse(NewYorkIso.location, e['hour_beginning'])),
+      e['price'])));
+}
 
 extension CaisoLmpPriceExtension on Caiso {
   Future<TimeSeries<num>> getHourlyLmp(
@@ -65,7 +132,6 @@ extension IsoneLmpPriceExtension on IsoNewEngland {
   }
 }
 
-
 extension NyisoLmpPriceExtension on NewYorkIso {
   Future<TimeSeries<num>> getHourlyLmp(
       {required Market market,
@@ -84,6 +150,3 @@ extension NyisoLmpPriceExtension on NewYorkIso {
         e['price'])));
   }
 }
-
-
-
